5665c44d2e83022df7f5d77e0db42d2096c4a3c7
Mathias Kuhlmey <m.kuhlmey@fm-maschinenbau.de>
2019-10-29 21:04:56 +0100

    Add entry point and define lib and exec at cmake

---
+++ source/blaengine.cpp
@@ -0,0 +1,15 @@
+#include "blaengine.h"
+
+using namespace blaengine;
+
+namespace blaengine {
+
+BlaEngine::BlaEngine() {}
+
+BlaEngine::~BlaEngine() {}
+
+void BlaEngine::GetEngineInfo() {}
+
+void BlaEngine::StartEngine() {}
+
+}  // namespace blaengine
