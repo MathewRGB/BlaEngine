3794e5f7113c71d03beee922d4c93915801d2966
Mathias Kuhlmey <m.kuhlmey@fm-maschinenbau.de>
2019-11-13 22:22:38 +0100

    Add test for setting realistic game state that still fails

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -8,26 +8,30 @@ bool compare_game_states(GameState state1, GameState state2);
 bool compare_game_states(GameState state1, GameState state2) {
   for (int i = 0; i < FIELD_NUMBER; i++) {
     if (state1.board.fields[i] != state2.board.fields[i]) {
-      cout << "board failed" << endl;
+      cout << "board comparison failed" << endl;
       return false;
     }
   }
   for (int i = 0; i < CASTLING_NUMBER; i++) {
     if (state1.board.castling[i] != state2.board.castling[i]) {
-      printf ("castling failed+%c+%c+", (char)state1.board.castling[i], (char)state2.board.castling[i]);
+      cout << "castling comparison failed" << endl;
       return false;
     }
   }
   if (state1.en_passant_field != state2.en_passant_field) {
+    cout << "en_passant_field is wrong" << endl;
     return false;
   }
   if (state1.half_moves_40_move_rule != state2.half_moves_40_move_rule) {
+    cout << "half_moves_40_move_rule is wrong" << endl;
     return false;
   }
   if (state1.next_half_move != state2.next_half_move) {
+    cout << "next_half_move is wrong" << endl;
     return false;
   }
   if (state1.next_turn != state2.next_turn) {
+    cout << "next_turn is wrong" << endl;
     return false;
   }
 
@@ -77,26 +81,23 @@ TEST(Calculator, test_set_fen_string) {
                                   comp_calc_module.current_game_state));
 }
 
-// TEST(Calculator, test_set_game_state) {
-//   string comparison_fen =
-//       "r2r2k1/pppqbppp/1nn1p3/8/1PPPN3/P3BN2/4QPPP/3R1RK1 b - b3 0 15";
-//   string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1";
-//   vector<string> moves{"e2e4", "d7d5", "e4d5", "g8f6", "d2d4", "f6d5",
-//                        "g1f3", "c8f5", "f1d3", "f5d3", "d1d3", "b8c6",
-//                        "e1g1", "e7e6", "d3e2", "f8e7", "c2c4", "d5b6",
-//                        "c1e3", "e8g8", "b1c3", "e7b4", "c3e4", "b4e7",
-//                        "a1d1", "d8d7", "a2a3", "f8d8", "b2b4"};
+TEST(Calculator, test_set_game_state) {
+  auto comparison_fen =
+      "r2r2k1/pppqbppp/1nn1p3/8/1PPPN3/P3BN2/4QPPP/3R1RK1 b - b3 0 15";
+  auto fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1";
+  vector<string> moves{"e2e4", "d7d5", "e4d5", "g8f6", "d2d4", "f6d5",
+                       "g1f3", "c8f5", "f1d3", "f5d3", "d1d3", "b8c6",
+                       "e1g1", "e7e6", "d3e2", "f8e7", "c2c4", "d5b6",
+                       "c1e3", "e8g8", "b1c3", "e7b4", "c3e4", "b4e7",
+                       "a1d1", "d8d7", "a2a3", "f8d8", "b2b4"};
 
-//   auto comp_calc_module = new Calculator();
-//   comp_calc_module->interpretAndSetFen(comparison_fen);
+  auto comp_calc_module = Calculator();
+  comp_calc_module.interpretAndSetFen(comparison_fen);
 
-//   auto calc_module = new Calculator();
-//   calc_module->interpretAndSetFen(fen);
-//   calc_module->makeMovesFromFieldStrings(moves);
+  auto calc_module = Calculator();
+  calc_module.interpretAndSetFen(fen);
+  calc_module.makeMovesFromFieldStrings(moves);
 
-//   ASSERT_TRUE(compare_game_states(calc_module.current_game_state,
-//                                   comp_calc_module.current_game_state));
-
-//   comp_calc_module->~Calculator();
-//   calc_module->~Calculator();
-// }
+  ASSERT_TRUE(compare_game_states(calc_module.current_game_state,
+                                  comp_calc_module.current_game_state));
+}
