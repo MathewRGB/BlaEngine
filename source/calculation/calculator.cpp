7347501cad6d09c07b6c5a56d3e3759bb17986f9
Mathias Kuhlmey <m.kuhlmey@fm-maschinenbau.de>
2019-11-18 23:34:48 +0100

    Improve calculator.cpp readability

--- source/calculation/calculator.cpp
+++ source/calculation/calculator.cpp
@@ -11,58 +11,60 @@ Calculator::~Calculator() {}
 void Calculator::interpretAndSetFen(string fen) {
   this->validateFenString(fen);
 
+  GameStateController& gstate_controller = this->game_state_controller;
+  GameState& current_gstate = this->game_state_controller.current_game_state;
+
   string fen_position = fen.substr(0, fen.find(" "));
-  this->game_state_controller.extractFenPosition(fen_position);
+  gstate_controller.extractFenPosition(fen_position);
 
   fen.erase(0, fen.find(" ") + 1);
-  this->game_state_controller.current_game_state.next_turn =
+  current_gstate.next_turn =
       (fen[0] == 'w') ? NextTurn::white : NextTurn::black;
 
   fen.erase(0, fen.find(" ") + 1);
   string castling_info = fen.substr(0, fen.find(" "));
   if (castling_info.compare("-") != 0) {
-    this->game_state_controller.extractFenCastling(castling_info);
+    gstate_controller.extractFenCastling(castling_info);
   }
 
   fen.erase(0, fen.find(" ") + 1);
   string en_passant = fen.substr(0, fen.find(" "));
   if (en_passant[0] != '-') {
-    this->game_state_controller.current_game_state.en_passant_field =
-        this->game_state_controller.getFieldIndex(en_passant);
+    current_gstate.en_passant_field =
+        gstate_controller.getFieldIndex(en_passant);
   }
 
   fen.erase(0, fen.find(" ") + 1);
   string rule_40_moves = fen.substr(0, fen.find(" "));
-  this->game_state_controller.current_game_state.half_moves_for_draw =
-      std::stoi(rule_40_moves);
+  current_gstate.half_moves_for_draw = std::stoi(rule_40_moves);
 
   fen.erase(0, fen.find(" ") + 1);
   string next_full_move = fen.substr(0, fen.find(" "));
-  this->game_state_controller.current_game_state.next_half_move +=
-      std::stoi(next_full_move) * 2;
-  if (this->game_state_controller.current_game_state.next_turn ==
-      NextTurn::white) {
-    this->game_state_controller.current_game_state.next_half_move--;
+  current_gstate.next_half_move += std::stoi(next_full_move) * 2;
+  if (current_gstate.next_turn == NextTurn::white) {
+    current_gstate.next_half_move--;
   }
 }
 
 void Calculator::makeMovesFromFieldStrings(vector<string> moves) {
+  GameStateController& gstate_controller = this->game_state_controller;
+  GameState& current_gstate = this->game_state_controller.current_game_state;
+
   for (uint i = 0; i < moves.size(); i++) {
     this->validateMoveString(moves[i]);
-    ushort field_before =
-        this->game_state_controller.getFieldIndex(moves[i].substr(0, 2));
-    ushort field_after =
-        this->game_state_controller.getFieldIndex(moves[i].substr(2, 3));
+    string field_string_before = moves[i].substr(0, 2);
+    string field_string_after = moves[i].substr(2, 3);
+
+    ushort field_before = gstate_controller.getFieldIndex(field_string_before);
+    ushort field_after = gstate_controller.getFieldIndex(field_string_after);
 
     Pieces piece_got = this->game_state_controller.transformPiece(moves[i]);
-    this->game_state_controller.makeMove(field_before, field_after, piece_got);
+    gstate_controller.makeMove(field_before, field_after, piece_got);
 
-    this->game_state_controller.current_game_state.next_half_move++;
-    this->game_state_controller.current_game_state.next_turn =
-        (this->game_state_controller.current_game_state.next_turn ==
-         NextTurn::white)
-            ? NextTurn::black
-            : NextTurn::white;
+    current_gstate.next_half_move++;
+    current_gstate.next_turn = (current_gstate.next_turn == NextTurn::white)
+                                   ? NextTurn::black
+                                   : NextTurn::white;
   }
 }
 
