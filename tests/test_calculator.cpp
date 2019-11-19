4cd1b6bd8fc3eb2ed40934d200ae5d122d6247a6
Mathias Kuhlmey <m.kuhlmey@fm-maschinenbau.de>
2019-11-19 01:16:45 +0100

    Improve readability of test_calculator

--- tests/test_calculator.cpp
+++ tests/test_calculator.cpp
@@ -40,77 +40,48 @@ bool compare_game_states(GameState state1, GameState state2) {
 
 TEST(Calculator, test_set_fen_string) {
   auto calc_module = Calculator();
+  GameState& current_game_state =
+      calc_module.game_state_controller.current_game_state;
   auto fen = "r2r2k1/pppqbppp/1nn1p3/8/1PPPN3/P3BN2/4QPPP/3R1RK1 b - b3 0 15";
   calc_module.interpretAndSetFen(fen);
 
   auto comp_calc_module = Calculator();
-  comp_calc_module.game_state_controller.current_game_state.board.fields[3] =
-      'R';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[5] =
-      'R';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[6] =
-      'K';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[12] =
-      'Q';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[13] =
-      'P';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[14] =
-      'P';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[15] =
-      'P';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[16] =
-      'P';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[20] =
-      'B';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[21] =
-      'N';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[25] =
-      'P';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[26] =
-      'P';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[27] =
-      'P';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[28] =
-      'N';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[41] =
-      'n';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[42] =
-      'n';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[44] =
-      'p';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[48] =
-      'p';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[49] =
-      'p';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[50] =
-      'p';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[51] =
-      'q';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[52] =
-      'b';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[53] =
-      'p';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[54] =
-      'p';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[55] =
-      'p';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[56] =
-      'r';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[59] =
-      'r';
-  comp_calc_module.game_state_controller.current_game_state.board.fields[62] =
-      'k';
-  comp_calc_module.game_state_controller.current_game_state.en_passant_field =
-      17;
-  comp_calc_module.game_state_controller.current_game_state
-      .half_moves_for_draw = 0;
-  comp_calc_module.game_state_controller.current_game_state.next_turn =
-      NextTurn::black;
-  comp_calc_module.game_state_controller.current_game_state.next_half_move = 30;
+  GameState& comp_game_state =
+      comp_calc_module.game_state_controller.current_game_state;
+  comp_game_state.board.fields[3] = 'R';
+  comp_game_state.board.fields[5] = 'R';
+  comp_game_state.board.fields[6] = 'K';
+  comp_game_state.board.fields[12] = 'Q';
+  comp_game_state.board.fields[13] = 'P';
+  comp_game_state.board.fields[14] = 'P';
+  comp_game_state.board.fields[15] = 'P';
+  comp_game_state.board.fields[16] = 'P';
+  comp_game_state.board.fields[20] = 'B';
+  comp_game_state.board.fields[21] = 'N';
+  comp_game_state.board.fields[25] = 'P';
+  comp_game_state.board.fields[26] = 'P';
+  comp_game_state.board.fields[27] = 'P';
+  comp_game_state.board.fields[28] = 'N';
+  comp_game_state.board.fields[41] = 'n';
+  comp_game_state.board.fields[42] = 'n';
+  comp_game_state.board.fields[44] = 'p';
+  comp_game_state.board.fields[48] = 'p';
+  comp_game_state.board.fields[49] = 'p';
+  comp_game_state.board.fields[50] = 'p';
+  comp_game_state.board.fields[51] = 'q';
+  comp_game_state.board.fields[52] = 'b';
+  comp_game_state.board.fields[53] = 'p';
+  comp_game_state.board.fields[54] = 'p';
+  comp_game_state.board.fields[55] = 'p';
+  comp_game_state.board.fields[56] = 'r';
+  comp_game_state.board.fields[59] = 'r';
+  comp_game_state.board.fields[62] = 'k';
+  comp_game_state.en_passant_field = 17;
+  comp_game_state.half_moves_for_draw = 0;
+  comp_game_state.next_turn = NextTurn::black;
+  comp_game_state.next_half_move = 30;
 
-  ASSERT_TRUE(compare_game_states(
-      calc_module.game_state_controller.current_game_state,
-      comp_calc_module.game_state_controller.current_game_state));
+  ASSERT_TRUE(compare_game_states(current_game_state, comp_game_state));
 }
 
 TEST(Calculator, test_set_game_state_2_moves) {
