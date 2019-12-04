938ebe7491f82333155c36dc23fccec0e73b2e76
MathewRGB <kuhlmey.m@gmail.com>
2019-12-04 20:24:36 +0100

    Provide tests for evaluateGameState method

--- tests/test_evaluator.cpp
+++ tests/test_evaluator.cpp
@@ -3,7 +3,7 @@
 
 using namespace blaengine::calculation;
 
-TEST(Evaluator, test_evaluate_game_state)
+TEST(Evaluator, test_evaluate_game_state_1)
 {
   auto evaluator = Evaluator();
   auto gstate_translator = GSateTranslator();
@@ -14,4 +14,56 @@ TEST(Evaluator, test_evaluate_game_state)
   int rating = evaluator.evaluateGameState(game_state);
 
   ASSERT_EQ(rating, 0);
+}
+
+TEST(Evaluator, test_evaluate_game_state_2)
+{
+  auto evaluator = Evaluator();
+  auto gstate_translator = GSateTranslator();
+  auto fen = "1nbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/1NBQKBNR w KQkq - 0 1";
+  auto& game_state = gstate_translator.gstate_controller.current_game_state;
+  
+  gstate_translator.interpretAndSetFen(fen);
+  int rating = evaluator.evaluateGameState(game_state);
+
+  ASSERT_EQ(rating, 0);
+}
+
+TEST(Evaluator, test_evaluate_game_state_3)
+{
+  auto evaluator = Evaluator();
+  auto gstate_translator = GSateTranslator();
+  auto fen = "rnbqkbnr/pppp1ppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  auto& game_state = gstate_translator.gstate_controller.current_game_state;
+  
+  gstate_translator.interpretAndSetFen(fen);
+  int rating = evaluator.evaluateGameState(game_state);
+
+  ASSERT_GT(rating, 0);
+}
+
+TEST(Evaluator, test_evaluate_game_state_4)
+{
+  auto evaluator = Evaluator();
+  auto gstate_translator = GSateTranslator();
+  auto fen = "1nb1kbnr/pppppppp/8/8/8/8/PPPPPPPP/1NBQKBNR w KQkq - 0 1";
+  auto& game_state = gstate_translator.gstate_controller.current_game_state;
+  
+  gstate_translator.interpretAndSetFen(fen);
+  int rating = evaluator.evaluateGameState(game_state);
+
+  ASSERT_LT(rating, 0);
+}
+
+TEST(Evaluator, test_evaluate_game_state_5)
+{
+  auto evaluator = Evaluator();
+  auto gstate_translator = GSateTranslator();
+  auto fen = "1nb1kbnr/pppppppp/8/8/8/8/PPPPPPPP/1NB1KB1R w KQkq - 0 1";
+  auto& game_state = gstate_translator.gstate_controller.current_game_state;
+  
+  gstate_translator.interpretAndSetFen(fen);
+  int rating = evaluator.evaluateGameState(game_state);
+
+  ASSERT_GT(rating, 0);
 }
