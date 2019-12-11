1e9ebc2801297cdf6405549e9f21c88524ed53c8
MathewRGB <kuhlmey.m@gmail.com>
2019-12-11 12:15:42 +0100

    Add searching depth as command line argument

--- source/main.cpp
+++ source/main.cpp
@@ -7,9 +7,10 @@
 using BlaEngine = blaengine::BlaEngine;
 using Uci = blaengine::communication::Uci;
 
-int main()
+int main(int argc, char *argv[])
 {
-  auto blaengine = make_shared<BlaEngine>(5);
+  ushort searching_depth = (argc == 2) ? stoi(argv[1]) : 2;
+  auto blaengine = make_shared<BlaEngine>(searching_depth);
   auto uci_module = Uci(blaengine);
 
   thread uci_com_thread(uci_module);
